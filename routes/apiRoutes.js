35cf13e6184bccfcda332d52a72a78ee89bce0a3
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-12-02 20:58:12 -0500

    resolved bugs with delete route and get notes route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@
 
 const fs = require("fs");
 const uuid = require("uuid");
-const router = require("express").Router();
+const path = require("path");
+const express = require("express")
+const router = express.Router();
 
 
 router.get("/notes", (req, res) => {
@@ -16,14 +18,16 @@ router.post("/notes", (req, res) => {
     const addNote = req.body;
     addNote.id = uuid.v4();
     notes.push(addNote);
-    fs.writeFileSync("./db/db/json", JSON.stringify(notes));
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
     res.json(notes);
 })
 
+router.delete("/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deleteNote = notes.filter((delNote) => delNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote));
 
-
-
-
-
+    res.json({ok: true});
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 
 const express = require("express");
-const path = require("path");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
@@ -14,33 +13,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
-
-app.delete("/api/notes/:id", (req, res) => {
-   fs.readFile('./db/db.json', 'utf8', (err, data) =>{
-    if (err) throw err;
-    const notes = JSON.parse(data)
-    const newNotes = notes.filter(notes => notes.id !== parseInt(req.params.id));
-
-    fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
-        res.json(newNotes);
-    });
-   }); 
-});
-
-app.get('/api/notes/:id', (req, res) => {
-    res.json(notes[req.params.id])
-})
-
- app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        res.json(notes);
-    })
- })
-
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
